97bb0a6484c2703d1e7f15c8b1b069e46e6495c5
kalangosecojf <kalangosecojf@gmail.com>
2021-08-18 01:29:03 -0300

    teste3

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>XYZ DaniloY</Text>
+      <Text>Danilo - Danilo</Text>
       <StatusBar style="auto" />
     </View>
   );
